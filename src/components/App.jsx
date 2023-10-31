04ce8932371c7b73880c4411501c3293c1c24454
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-10-31 13:19:54 +0100

    render Searchbar add App State & ApiKey

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './Searchbar/Searchbar';
+
+
+export default class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+    isLoading: false,
+    showModal: false,
+    modalImageURL: '',
+  };
+
+  apiKey = '39267402-49695b078cc30e5676dab55fe';
+  apiUrl = 'https://pixabay.com/api/';
+
+
+  render() {
+    const { images, isLoading, showModal, modalImageURL } = this.state;
+
+    return (
+      <Searchbar onSubmit={this.handleSearchSubmit} />
+    
+    )
+
+}
+}
