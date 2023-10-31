76e89e1ba29c5f300e771feb145599d2cf6b63ff
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-10-31 13:19:07 +0100

    create searchbar component
    handleChange handleSubmit

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+export default class Searchbar extends Component{
+state={
+    query:'',
+};
+handleChange = (e) => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+render(){
+    return(
+        <form onSubmit={this.handleSubmit} className="form">
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+    )
+}
+}
