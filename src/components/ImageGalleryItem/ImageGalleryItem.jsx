d59cfd07cd86bca3b2fe7bab209f44657785a38c
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-02 09:05:54 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,7 @@
 export const ImageGalleryItem = ({ id, webformatURL, largeImageURL, onOpenModal }) => {
     return (
       <li className="gallery-item" onClick={() => onOpenModal(largeImageURL)}>
-        <img src={webformatURL} alt={`Image ${id}`} />
+        <img src={webformatURL} alt={`${id}`} />
       </li>
     );
   };
